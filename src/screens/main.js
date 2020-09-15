26d2eb387dfcb0077c6bfb2a5250879376579b4c
ashleighrae <67845170+ashleighrae@users.noreply.github.com>
2020-09-16 09:36:44 +1000

    Setup page titles and styling.
    Loads webfont into CSS file and import into style.js. Setup titles for buttons on main (home) page.

--- src/screens/main.js
+++ src/screens/main.js
@@ -4,22 +4,13 @@ import {View, Text, TouchableHighlight, TouchableOpacity} from 'react-native';
 //This is how you import the style sheet styles
 // If another stylesheet is wanted add it with a , separator
 // import {styles, another, more, soManyStyles} from ...
-import {buttons, styles} from '../styles/styles'
-
-
+import {buttons, styles, page} from '../styles/styles'
 
 class Main extends Component {
     render() {
         return (
             <View style={styles.center}>
-                <Text style={styles.title}>Main</Text>
-                <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('Pages')}
-                >
-                    <View style={buttons.primary}>
-                        <Text style={buttons.buttonText}>Pages</Text>
-                    </View>
-                </TouchableOpacity>
+                <Text style={page.title}>Home Page</Text>
                 <TouchableOpacity
                     onPress={() => this.props.navigation.navigate('Library')}
                 >
@@ -28,17 +19,17 @@ class Main extends Component {
                     </View>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('Login')}
+                    onPress={() => this.props.navigation.navigate('Library')}
                 >
                     <View style={buttons.primary}>
-                        <Text style={buttons.buttonText}>Login</Text>
+                        <Text style={buttons.buttonText}>Stripes Earned</Text>
                     </View>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('Draw')}
+                    onPress={() => this.props.navigation.navigate('Login')}
                 >
                     <View style={buttons.primary}>
-                        <Text style={buttons.buttonText}>Draw</Text>
+                        <Text style={buttons.buttonText}>Video Tutorial</Text>
                     </View>
                 </TouchableOpacity>
             </View>
