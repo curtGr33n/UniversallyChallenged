7964971ca623ddca9907c0fb7c4b4ffdab4b68b6
dhenl2 <69876000+dhenl2@users.noreply.github.com>
2020-09-15 20:55:54 +1000

    Attempting to place completed drawn canvas begin canvas
    Canvas background is set to transparent but its actually not transparent?
    halp

--- src/screens/Draw2.js
+++ src/screens/Draw2.js
@@ -1,66 +1,79 @@
 import React, {useRef, useState} from 'react';
-import { StyleSheet, TextInput, View, Image } from 'react-native';
-import * as FileSystem from 'expo-file-system';
+import { StyleSheet, TextInput, View, Image, TouchableOpacity } from 'react-native';
 import SignatureScreen, {readSignature} from 'react-native-signature-canvas';
 
 const App = () => {
     const [signature, setSign] = useState(null);
     const [value, onChangeText] = useState('Useless Placeholder');
     const [desc, setDesc] = useState("please sign");
+    const [pen, setPen] = useState("rgb(2,57,255)");
+
+    const [ show, setShow ] = useState(false);
+    const ref = useRef();
+    const showSignature = () => setShow(true);
+    const [background, setBackground] = useState("rgba(252,3,3,0.0)")
+
+    const canvasStyle = `
+                        .m-signature-pad {
+                        width: 600px;
+                        height: 350px;
+                        margin-left: 0px;
+                        margin-top: 0px;
+                        background-color: transparent
+                        }
+                        // .m-signature-pad--footer {
+                        //     display: none
+                        // }
+                        .m-signature-pad--body {
+                            left: 0px;
+                            right: 0px;
+                            top: 0px;
+                            bottom: 0px;
+                            border: 0px solid #f4f4f4;
+                        }
+                        .m-signature-pad--body
+                            canvas {
+                            position: absolute;
+                            left: 0;
+                            top: 0;
+                            width: 100%;
+                            height: 100%;
+                            border-radius: 0px;
+                            box-shadow: 0 0 0px rgba(0, 0, 0, 0.02) inset;
+                            }
+                        `;
 
     const handleSignature = signature => {
-        // console.log(signature);
-        console.log("Handled signature in top lvl method")
+        // ref.current.readSignature();
+        console.log("Handled Signature in Sign")
+        // console.log("\n" + signature + "\n");
         setSign(signature);
-        setDesc("sign success");
+        // ref.current.readSignature();
+        setShow(false);
     };
 
-    const Sign = ({text, onOK}) => {
-        const [ show, setShow ] = useState(false);
-        const ref = useRef();
-        const showSignature = () => setShow(true);
-        const [background, setBackground] = useState("rgba(252,3,3,0.5)")
+    const handleEmpty = () => {
+        console.log('Empty');
+    }
 
-        const handleSignature = signature => {
-            // console.log(signature);
-            console.log("Handled Signature in Sign")
-            onOK(signature);
-            setShow(false);
-        };
+    const handleClear = () => {
+        console.log('clear success!');
+    }
 
-        const handleEmpty = () => {
-            console.log('Empty');
-        }
+    const handleEnd = () => {
+        console.log("End Stroke")
+        ref.current.readSignature();
+        setSign(signature);
+        console.log("\n" + signature + "\n");
+    }
 
-        const handleClear = () => {
-            console.log('clear success!');
-        }
+    const handleBegin = () => {
+        console.log('Begin Stroke');
+    };
 
-        const handleEnd = () => {
-            ref.current.readSignature();
-            setBackground(signature)
-        }
-
-        const handleBegin = () => {
-            console.log('begin!');
-        };
-
-        return (
-            <View style={styles.container}>
-                <SignatureScreen
-                    ref={ref}
-                    onEnd={handleEnd}
-                    // onOK={handleSignature}
-                    onEmpty={handleEmpty}
-                    onClear={handleClear}
-                    onBegin={handleBegin}
-                    autoClear={false}
-                    descriptionText={text}
-                    backgroundColor={background}
-                    penColor={"rgb(2,57,255)"}
-                />
-            </View>
-        );
+    function changeColor (color) {
+        setPen(color);
+        setBackground(signature);
     }
 
     return (
@@ -69,26 +82,43 @@ const App = () => {
                 {signature ? (
                     <Image
                         resizeMode={"contain"}
-                        style={{ width: 750, height: 100 }}
+                        style={{ width: 750, height: 350 }}
                         source={{ uri: signature }}
                     />
                 ) : null}
             </View>
-            <Sign
-                key="sign1"
-                onOK={handleSignature}
-                text={desc}
-            />
+            <View style={styles.container}>
+                <View style={styles.container}>
+                    <TouchableOpacity
+                        style={[styles.button, styles.red]}/>
+                </View>
+                <Image
+                    source={{uri: signature}}
+                    style={{width:600, height: 350, position: 'absolute',
+                           right: 200}} />
+                <SignatureScreen
+                    ref={ref}
+                    onEnd={handleEnd}
+                    onOK={handleSignature}
+                    onEmpty={handleEmpty}
+                    onClear={handleClear}
+                    onBegin={handleBegin}
+                    autoClear={false}
+                    descriptionText="Sign here"
+                    backgroundColor={background}
+                    penColor={pen}
+                    webStyle={canvasStyle}
+                />
+            </View>
         </View>
     );
 }
-
 export default App;
 
 const styles = StyleSheet.create({
     preview: {
         height: 114,
-        backgroundColor: "#F8F8F8",
+        backgroundColor: "rgba(252,3,3,0.0)",
         justifyContent: "center",
         alignItems: "center",
         marginTop: 15,
@@ -108,8 +138,21 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
+        flexDirection: 'row',
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    button: {
+        height: 110,
+        width: 110,
+        marginHorizontal: 20,
+        marginVertical: 20
+    },
+    red: {
+        backgroundColor: '#fc0703'
+    },
+    blue: {
+        backgroundColor: '#0422e0'
     }
 });
