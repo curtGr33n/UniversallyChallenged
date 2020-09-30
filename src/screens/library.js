72eccfa3ff29d3cb509301b7b0ac9d6a2c2fda30
Jeremy <j.dostalsharp@uqconnect.edu.au>
2020-10-01 09:52:54 +1000

    Books in library now navigate to pages screen
    Books clicked on in the library now take you to a new pages screen.

--- src/screens/library.js
+++ src/screens/library.js
@@ -10,14 +10,7 @@ import {TouchableOpacity,TouchableHighlight, Image}  from "react-native";
 //This is how you import the style sheet
 import {styles, buttons, page} from '../styles/styles.js';
 
-const Book = ({size, margin, text}) => (
-    <View style={[styles.bookTest, {width: size, height: size, marginHorizontal: margin}]}>
-        <Text style={styles.logoText}>{text}</Text>
-    </View>
-)
-
 class Library extends Component {
-
     getData = async (values) => {
         try {
             console.log(values.classId);
@@ -84,28 +77,13 @@ class Library extends Component {
                         {/*This is where the drop down menus are going*/}
 
                     </View>
-                        <BooksList/>
-                    {/*<ScrollView style={{
-                        backgroundColor: "white",
-                        flex: 10
-                    }}>
-                        {this.booksList()}
-                    </ScrollView>*/}
+                    <BooksList/>
                 </View>
                 <this.MyReactNativeForm />
             </View>
         )
     }
     /* Returns the list of book objects to be displayed in the library */
-    booksList() {
-        return this.state.books.map((book) => {
-            return (
-                <View>
-                    {book.book}
-                </View>
-            )
-        })
-    }
 }
 
 export default Library;
