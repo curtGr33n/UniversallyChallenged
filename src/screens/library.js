75c3f818d7a47f0e718bfd2081ae1db7b5a49d6a
Jeremy <j.dostalsharp@uqconnect.edu.au>
2020-10-07 10:13:46 +1000

    Have basic library functonality working properly.
    Library now can call and display the list of books for class with classID: 1.

--- src/screens/library.js
+++ src/screens/library.js
@@ -1,32 +1,63 @@
-import React, { Component, useState } from 'react';
-import { View, Text, ScrollView} from 'react-native';
-import { Button, TextInput } from 'react-native';
+import React, { Component} from 'react';
+import { View, Text, TouchableOpacity, Button, TextInput } from 'react-native';
 import { Formik } from 'formik';
-import BooksList from '../components/booksList.js'
-import { Tile } from 'react-native-elements';
-import { SectionGrid } from 'react-native-super-grid';
-import {TouchableOpacity,TouchableHighlight, Image}  from "react-native";
+//import BooksList from '../components/booksListDeprecated.js'
 
 //This is how you import the style sheet
 import {styles, buttons, page} from '../styles/styles.js';
+import {useNavigation} from "@react-navigation/core";
+import {FlatGrid} from "react-native-super-grid";
+import {Tile} from "react-native-elements";
 
 class Library extends Component {
-    getData = async (values) => {
+
+    state = {
+        schoolId: 0,
+        classId: 1,
+        studentId: 0,
+        books: []
+    };
+
+    setStudentId = (id) => {
+        this.setState({studentId: id})
+    };
+
+    setClassroomId = (id) => {
+        this.setState({classId: id})
+    };
+
+    setSchoolId = (id) => {
+        this.setState({schoolId: id})
+    };
+
+
+    getData = async () => {
         try {
-            console.log(values.classId);
-            //console.log('deco3801-universally-challenged.uqcloud.net/getClassBooks?classId=' + values.classId);
-            //let response = await fetch('deco3801-universally-challenged.uqcloud.net/getClassBooks?classId=' + values.classId);
-            let response = await fetch('https://deco3801-universally-challenged.uqcloud.net/getClassBooks?classId=' + values.classId);
+            console.log(this.state.classId);
+            let response = await fetch('https://deco3801-universally-challenged.uqcloud.net/getClassBooks?classId=' + this.state.classId);
             if (response.ok) {
-                console.log(response);
                 let juice = await response.text();
-                console.log(juice);
+                let data = JSON.parse(juice)
+                this.setState({books:data
+                })
             } else {
                 alert("HTTP-Error: " + response.status);
             }
         } catch (error) {
             console.error(error);
         }
+    };
+
+    getLibraryBooks = () => {
+        /* this function needs to be ale to call all the books from
+        the server for the school, class, student and set books to the array
+        of objects*/
+        let booksList =  this.getData()
+
+        /* For future reference to add to a state list use this
+        this.setState(prevState => ({ books: [...prevState.books, 'Test']
+        }))
+        */
     };
 
     MyReactNativeForm = props => (
@@ -53,7 +84,6 @@ class Library extends Component {
     );
 
     render() {
-
         return (
             <View style={{
                 backgroundColor: "gold",
@@ -73,11 +103,38 @@ class Library extends Component {
                 }}>
                     <View style={{
                         flex: 0.1,
+                        flexDirection: 'row',
+                        justifyContent: "space-around",
                     }}>
                         {/*This is where the drop down menus are going*/}
+                        <View>
 
+                        </View>
+                        <View>
+
+                        </View>
+                        <TouchableOpacity onPress={() => this.getData()}
+                                          style={styles.bookText}>
+                            <Text>Touch me baby, please</Text>
+                        </TouchableOpacity>
                     </View>
-                    <BooksList/>
+                    <FlatGrid
+                        itemDimension={300}
+                        data={this.state.books}
+                        style={styles.gridView}
+                        spacing={10}
+                        renderItem={({item}) => (
+                            <View style={styles.itemContainer}>
+                                <Tile imageSrc={require('../assets/place-holder-open-book.png')}
+                                      containerStyle={styles.image}
+                                      title={item.bookTitle}
+                                      titleStyle={styles.bookText}
+                                      style={styles.bookText}
+                                      onPress={page}
+                                />
+                            </View>
+                        )}
+                    />
                 </View>
                 <this.MyReactNativeForm />
             </View>
