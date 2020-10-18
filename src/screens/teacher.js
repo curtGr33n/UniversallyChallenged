07b9ac434c549def2dc8b548dbc74691b0011199
curtgreen <c.green@uqconnect.edu.au>
2020-10-18 14:27:10 +1000

    teacher controls now have permissions set

--- src/screens/teacher.js
+++ src/screens/teacher.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {styles, buttons, page} from '../styles/styles.js';
 
+/**
+ * Displays the teacher option screen
+ */
 class Teacher extends Component {
     render() {
         return (
             <View style={styles.container}>
-
                 <Text style={page.title}>Teacher Settings</Text>
                 <View style={[page.homeButtons, page.offsetData]}>
                     <TouchableOpacity
@@ -15,9 +17,7 @@ class Teacher extends Component {
                     >
                         <Image source={require('../assets/images/library.png')} style={page.image}/>
                         <Text style={buttons.buttonText}>Add Book</Text>
-
                     </TouchableOpacity>
-
                     <TouchableOpacity
                         onPress={() => this.props.navigation.navigate('editBook')}
                         style={page.primary}
@@ -26,7 +26,6 @@ class Teacher extends Component {
                         <Text style={buttons.buttonText}>Add Page & Creators</Text>
                     </TouchableOpacity>
                 </View>
-
             </View>
         )
     }
