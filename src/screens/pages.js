92f551ab36462500bf30f4f070938ec49ed877ad
Jeremy <j.dostalsharp@uqconnect.edu.au>
2020-10-21 11:06:06 +1000

    Functions in place for creator final images
    For each creator if they have a final image for a page, this will sent to showBook as a base64 string
    If the page is no longer active then it will send through the page.finalImage through to showBook as a base64 string
    If something goes wrong it will send through buzz as a base64 string to showbooks

--- src/screens/pages.js
+++ src/screens/pages.js
@@ -5,7 +5,15 @@ import {styles, buttons, page} from '../styles/styles.js';
 import {TouchableOpacity,TouchableHighlight, Image, Button}  from "react-native";
 import Draw from '../components/draw.js'
 import ShowBooks from '../components/showBook.js'
+import { returnBuzz64String } from "../assets/buzz64";
 
+
+/***
+ * A screen to display either the Canvas or the created image of the book
+ * @param book - A book object from the JSON wiki
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const Pages = (book) => {
     const bookId = book.route.params.bookId;
     //console.log("BookId: " + bookId);
@@ -13,7 +21,50 @@ const Pages = (book) => {
     const [pages, setPages] = useState(book.route.params.pages);
     const [page, setPage] = useState(pages[pageNumber])
     const [storyTitle, setStoryTitle] = useState(book.route.params.bookTitle);
-    const [key, setKey] = useState(100)
+    const [key, setKey] = useState(1000)
+    const [creatorFinal, setCreatorFinal] = useState(false)
+    const [imageString, setImageString] = useState("")
+
+    /* Check to see if creator has finished their contribution for that page */
+    function checkCreatorFinal() {
+        page.creators.map((item) => {
+            if (item.studentId == global.id) {
+                if (!(item.canvas === "")) {
+                    setCreatorFinal(true)
+                    setImageString(item.canvas)
+                }
+            }
+        })
+    }
+
+    /* Sets the imageString to the correct base 64 image string or to a default string
+      if image string is non existent
+     */
+    function setFinalImageString() {
+        if (creatorFinal) {
+            if (!(page.active)) {
+                setImageString(page.finalImage)
+            } else if (imageString === "") {
+                page.creators.map((item) => {
+                    if (item.studentId == global.id) {
+                        if (!(item.canvas === "")) {
+                            setImageString(item.canvas)
+                        } else {
+                            /*Set default base64 image string because something went
+                              wrong above
+                             */
+                            setImageString(returnBuzz64String())
+                        }
+                    }
+                })
+            } else {
+                /*Set default base64 image string because something went
+                  wrong above
+                 */
+                setImageString(returnBuzz64String())
+            }
+        }
+    }
 
     /*This will change the page and update pageNumber */
     function changePage(value) {
@@ -21,6 +72,8 @@ const Pages = (book) => {
             setPage(pages[pageNumber + 1])
             setPageNumber((prevState) => prevState + 1)
             setKey((prevState) => prevState + 1)
+            checkCreatorFinal()
+            setFinalImageString()
         }
         else if (value === 'increment' && pageNumber == pages.length - 1) {
             setPage(pages[pages.length - 1])
@@ -29,6 +82,8 @@ const Pages = (book) => {
             setPage(pages[pageNumber - 1])
             setPageNumber((prevState) => prevState - 1)
             setKey((prevState) => prevState - 1)
+            checkCreatorFinal()
+            setFinalImageString()
         }
         else if (value === 'decrement' && pageNumber == 0) {
             setPage(pages[0])
@@ -48,16 +103,15 @@ const Pages = (book) => {
                 justifyContent:'center',
                 alignItems: 'center'
             }}>
-                {page.active
+                {creatorFinal && page.active
                     ? <Draw
                         bookId={bookId}
                         pageId={pageNumber}
                         page={page}
                         key={key}
                     />
-                    : <ShowBooks pageNum={pageNumber}/>
+                    : <ShowBooks pageNum={pageNumber} imageString={imageString}/>
                 }
-
             </View>
             <View style={{
                 backgroundColor: "#f8ebc4",
