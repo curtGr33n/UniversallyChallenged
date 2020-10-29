13496903a27f4ae3fcd1dd60fe5ba1ba498e80b3
ashleighrae <67845170+ashleighrae@users.noreply.github.com>
2020-10-29 10:39:46 +1000

    Pop-ups disabled.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-// import {YellowBox} from 'react-native';
-//
-// YellowBox.ignoreWarnings(['Warning: ...']);
-//
-// console.disableYellowBox = true;
+import {YellowBox} from 'react-native';
+
+YellowBox.ignoreWarnings(['Warning: ...']);
+
+console.disableYellowBox = true;
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
