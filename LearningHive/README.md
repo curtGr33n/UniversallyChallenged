2a531add41c5a91c4e15678713670137750ce955
Jeremy <j.dostalsharp@uqconnect.edu.au>
2020-12-15 10:21:26 +1000

    Successfully rebased app
    Rebased app so there are no expo dependencies

--- LearningHive/README.md
+++
@@ -1,2 +0,0 @@
-# Learning Hive
-Drawing, sharing and collaborating program for children with ADS
--- LearningHive/index.js
+++
@@ -1,6 +0,0 @@
-import App from './App';
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-
-AppRegistry.registerComponent(appName, () => App);
